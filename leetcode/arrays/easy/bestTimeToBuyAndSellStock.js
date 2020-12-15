c5bf8a5d51a4caa2d11e0d3e6977d0d32b8f390d
Caro <55109108+CaroMen@users.noreply.github.com>
2020-12-15 17:34:06 -0600

    easy arrays

--- leetcode/arrays/easy/bestTimeToBuyAndSellStock.js
+++ leetcode/arrays/easy/bestTimeToBuyAndSellStock.js
@@ -18,3 +18,16 @@
 // Input: [7,6,4,3,1]
 // Output: 0
 // Explanation: In this case, no transaction is done, i.e. max profit = 0.
+
+
+var maxProfit = function (prices) {
+    let profit = 0; // set profit to 0
+    let buyPrice = prices[0]; // set buyPrice to the very first index
+
+    for (let i = 1; i < prices.length; i++) { // we loop starting at the second index of the prices array
+        buyPrice = Math.min(prices[i], buyPrice); // we compare the current price to our variable buyPrice, which is the first value and get the minimum
+        profit = Math.max(prices[i] - buyPrice, profit) //
+    }
+
+    return profit;
+}
