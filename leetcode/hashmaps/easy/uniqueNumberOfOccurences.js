591823a09a540e26b66f5cf1d39f113397d372bb
Caro <55109108+CaroMen@users.noreply.github.com>
2021-01-04 18:49:09 -0600

    new methods and algorithm

--- leetcode/hashmaps/easy/uniqueNumberOfOccurences.js
+++ leetcode/hashmaps/easy/uniqueNumberOfOccurences.js
@@ -17,5 +17,26 @@
 // Output: true
 
 var uniqueOccurrences = function (arr) {
+    let map = {}; // {'1': 3, '2': 2, '3': 1}
 
-}
+    // move array values to object.
+    for (let val of arr) {
+
+        // if the value in array is in map then increment value.
+        // else add it with a value of 1
+        if (map[val]) {
+            map[val]++;
+        } else {
+            map[val] = 1;
+        }
+    }
+
+    // we create an array of the map object
+    let entries = Object.entries(map); // [3, 2, 1]
+
+
+    return (new Set(entries)).size === entries.length; // Set(3) {3, 2, 1}. size for set is 3 and entries length is 3
+};
+
+// if a value in the array had the same occurence, then the Set size
+// would not be equal to the Object.entries length
