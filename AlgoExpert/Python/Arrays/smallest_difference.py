160818bf0f3d420597425108ca33b8ea6d817657
Caroline Mendez <caroline_mendez@alumni.baylor.edu>
2021-05-23 14:46:50 -0700

    partial solution

--- AlgoExpert/Python/Arrays/smallest_difference.py
+++ AlgoExpert/Python/Arrays/smallest_difference.py
@@ -1,2 +1,19 @@
 # write a function that takes in two non-empty arrays of integers, finds the pair of numbers (one from each array) whose absolute difference is closest to zero,
 # and returns an array containing these two numbers, with the number from the first array in the first position
+
+
+def smallest_difference(array_one, array_two):
+    array_one.sort()
+    array_two.sort()
+
+    idx_one = 0
+    idx_two = 0
+
+    smallest = float("inf")
+    current = float("inf")
+
+    smallest_pair = []
+
+    while idx_one < len(array_one) and idx_two < len(array_two):
+        first_num = array_one[idx_one]
+        second_num = array_two[idx_two]
