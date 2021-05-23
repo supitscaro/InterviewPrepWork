09b167770fc0f7d2fd092cedb99c8af6aa025a5f
Caroline Mendez <caroline_mendez@alumni.baylor.edu>
2021-05-23 14:41:31 -0700

    instructions added

---
+++ AlgoExpert/Python/Arrays/smallest_difference.py
@@ -0,0 +1,2 @@
+# write a function that takes in two non-empty arrays of integers, finds the pair of numbers (one from each array) whose absolute difference is closest to zero,
+# and returns an array containing these two numbers, with the number from the first array in the first position
