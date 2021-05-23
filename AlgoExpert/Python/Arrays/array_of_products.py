eed0686e9c4131a823f390a3938b40c323e47d9b
Caroline Mendez <caroline_mendez@alumni.baylor.edu>
2021-05-23 16:17:50 -0700

    instructions added

---
+++ AlgoExpert/Python/Arrays/array_of_products.py
@@ -0,0 +1,1 @@
+# write a function that takes in a non-empty array of integers and returns an array of the same length, where each element in the output array is equal to the product of every other number in the input array.
