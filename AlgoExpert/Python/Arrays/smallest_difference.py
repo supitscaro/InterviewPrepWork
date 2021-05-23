802af78704d118633443b98160aa7a297a8325f4
Caroline Mendez <caroline_mendez@alumni.baylor.edu>
2021-05-23 15:49:05 -0700

    finished solution

--- AlgoExpert/Python/Arrays/smallest_difference.py
+++ AlgoExpert/Python/Arrays/smallest_difference.py
@@ -17,3 +17,17 @@ def smallest_difference(array_one, array_two):
     while idx_one < len(array_one) and idx_two < len(array_two):
         first_num = array_one[idx_one]
         second_num = array_two[idx_two]
+
+        if first_num < second_num:
+            current = second_num - first_num
+            idx_one += 1
+        elif second_num < first_num:
+            current = first_num - second_num
+            idx_two += 1
+        else:
+            return [first_num, second_num]
+
+        if smallest > current:
+            smallest = current
+            smallest_pair = [first_num, second_num]
+    return smallest_pair
