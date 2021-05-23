41eb6255a9cea1a0a1f7f72e979424e91438aafe
Caroline Mendez <caroline_mendez@alumni.baylor.edu>
2021-05-23 12:20:35 -0700

    new problem instructions added

---
+++ AlgoExpert/Python/Arrays/three_number_sum.py
@@ -0,0 +1,3 @@
+# write a function that takes in a non-empty array of distinct integers and an integer representing a target sum
+# the function should find all triplets in the array that sum up to the target sum and return a two-dimensional array of all these triplets
+# the numbers in each triplet should be ordered in ascending order, and the triplets themselves should be ordered in ascending order with respect to the numbers they hold.
