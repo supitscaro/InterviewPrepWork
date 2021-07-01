17828d589960b9a61b9b94acab30c6de3f1ab9ee
Caro <55109108+CaroMen@users.noreply.github.com>
2021-06-30 23:55:08 -0500

    new BST problem started

---
+++ AlgoExpert/Python/BST/find_closest_value.py
@@ -0,0 +1,13 @@
+# write a function that takes in a BST and a target integer value and returns the closest value to that target value contained in the BST
+
+# you can assume there will only be one closest value
+
+def findClosestValueInBst(tree, target):
+    pass
+
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
