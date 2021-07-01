4fa7d87cbe6b159a2f7b264694a89f5c047f1bd6
Caro <55109108+CaroMen@users.noreply.github.com>
2021-07-01 11:24:22 -0500

    new bst problem added

--- AlgoExpert/Python/BST/find_closest_value.py
+++ AlgoExpert/Python/BST/find_closest_value.py
@@ -2,8 +2,38 @@
 
 # you can assume there will only be one closest value
 
+# INPUTS/OUTPUTS
+"""
+tree =        10
+            /    \
+           5     15
+         /  \   /  \
+        2    5 13  22
+       /         \
+      1           14
+
+target = 12
+"""
+
+# output => 13
+
+
 def findClosestValueInBst(tree, target):
-    pass
+    return findClosestValueHelper(tree, target, tree.value)
+
+
+def findClosestValueHelper(tree, target, closest):
+    current_node = tree
+    while current_node is not None:
+        if abs(target - closest) > abs(target - current_node.value):
+            closest = current_node.value
+        if target < current_node.value:
+            current_node = current_node.left
+        elif target > current_node.value:
+            current_node = current_node.right
+        else:
+            break
+    return current_node
 
 
 class BST:
