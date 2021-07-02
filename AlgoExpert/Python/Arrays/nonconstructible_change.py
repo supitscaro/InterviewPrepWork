e7a21400ac212cec17a16e52178f5fef87b7f1a1
Caro <55109108+CaroMen@users.noreply.github.com>
2021-07-01 21:25:32 -0500

    notes added to problem

--- AlgoExpert/Python/Arrays/nonconstructible_change.py
+++ AlgoExpert/Python/Arrays/nonconstructible_change.py
@@ -1,6 +1,23 @@
 # Given an array of positive integers representing the values of coins in your possession, write a function that returns the minimum amount of change (the minimum sum of money) that you cannot create.
 # The given coins can have any positive integer value and aren't neccessarily unique
 
+# INPUTS/OUTPUTS
+# [1, 1, 4] =>
+# change = 1
+# change = 1 + 1 = 2
+# change = 2 + 3 = 5
+# if coin is <= our previous change + 1, we can make all of the change previously
+# and all the change between what we can make previously and this new coin
+
+# SOLUTIONS
+# sort arr in place
+# variable 'change' equal to 0
+# for coin in coins:
+# if coin > change + 1:
+# return change + 1
+# change += coin
+# return change + 1
+
 def non_constructible_change(coins):
     coins.sort()
 
