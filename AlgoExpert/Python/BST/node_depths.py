0dffa2a6f543d45060e62687daa70811c474fd64
Caro <55109108+CaroMen@users.noreply.github.com>
2021-07-04 18:27:28 -0500

    new problem started

---
+++ AlgoExpert/Python/BST/node_depths.py
@@ -0,0 +1,9 @@
+# the distance between a node in a Binary Tree and the tree's root is called the node's depth
+
+# write a function that takes in a Binary Tree and returns the sum of its nodes' depths
+
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
