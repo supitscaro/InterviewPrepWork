a0b31cda88f62442ef919ebf0c1d52eedc4f2b1b
Caro <55109108+CaroMen@users.noreply.github.com>
2021-07-05 17:10:08 -0500

    new problem started

---
+++ AlgoExpert/Python/BST/binary_search.py
@@ -0,0 +1,6 @@
+# write a function that takes in a sorted array of integers as well as a target integer
+# the function should use the binary search algorithm to determine if the target integer is contained in the array
+# and should return its index if it is, otherwise -1
+
+# INPUTS/OUTPUTS
+# array = [0, 1, 21, 33, 45, 45, 61, 71, 72, 73], target = 33 => 3
