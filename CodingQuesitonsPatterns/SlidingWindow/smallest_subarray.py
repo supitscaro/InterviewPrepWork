9e21892452acfbceac8d1639f5dab5b0cfb1dde8
Caro <55109108+CaroMen@users.noreply.github.com>
2021-07-30 15:41:03 -0500

    second sliding window problem with notes

---
+++ CodingQuesitonsPatterns/SlidingWindow/smallest_subarray.py
@@ -0,0 +1,41 @@
+# Given an array of positive numbers and a positive number 'S', find the length of the smallest contigous subarray whose sum is greater than or equal to 'S'. Return 0 if no such subarray exists
+
+# INPUTS/OUPUTS
+# [2, 1, 5, 2, 3, 2], S = 7 => 2
+# explanation:
+#   the smallest subarray with a sum greater than or equal to '7' is [5, 2]
+
+# [2, 1, 5, 2, 8], S = 7 => 1
+# explanation:
+#   the smallest subarray with a sum greater than or equal to '7' is [8]
+
+# [3, 4, 1, 1, 6], S = 8 => 3
+# explanation:
+#   smallest subarrays with a sum greater than or equal to '8' are [3, 4, 1] or [1, 1, 6]
+
+# STEPS
+#   add up elements from beginning of array until their sum becomes greater than or equal to S
+
+#   these elements are our sliding window. we have to find the smallest window having a sum greater than or equal to S. the length of this window is the smallest window so far
+
+#   we will add one element in the sliding window in a stepwise fashion
+
+#   we will also try to shrink the window from the beginning. we will do this until the window's sum is smaller than S again. we need this to find the smallest window. the shrinking has to happen in multiple steps:
+#       check if current window length is smallest so far, and if it is, remember its length
+#       subtract first element of the window from the running sum to shrink the sliding window
+
+def smallest_subarray_with_given_sum(s, arr):
+    min_length = math.inf
+    window_start, window_sum = 0, 0
+
+    for idx in range(len(arr)):
+        min_length += arr[idx]
+
+        while idx >= s:
+            min_length = min(min_length, idx - window_start + 1)
+            window_sum -= arr[window_start]
+            window_start += 1
+        if min_length == math.inf:
+            return 0
+
+    return min_length
