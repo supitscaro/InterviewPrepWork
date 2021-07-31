4e07a3bfa115f221f6db7ece747619feb78bd40e
Caro <55109108+CaroMen@users.noreply.github.com>
2021-07-31 13:57:51 -0500

    new problem started

---
+++ CodingQuesitonsPatterns/SlidingWindow/fruits_into_basket.py
@@ -0,0 +1,9 @@
+# Given an array of characters where each character represents a fruit tree, you are given two baskets and your goal is to put maximum number of fruits in each basket
+# The only restriction is that each basket can have only one type of fruit
+
+# Write a function to return the maxium number of fruits in both baskets
+
+# INPUTS/OUTPUTS
+#   ['A', 'B', 'C', 'A', 'C] => 3
+#   explanation:
+#       we can put 2 'C' in one basket and one 'A' in the other from the subarray ['C', 'A', 'C']
