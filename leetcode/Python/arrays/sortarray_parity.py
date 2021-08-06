3a998b69e0d209993181c5dced84677eef8c28a9
Caro <55109108+CaroMen@users.noreply.github.com>
2021-08-06 10:20:16 -0500

    sorted in place

--- leetcode/Python/arrays/sortarray_parity.py
+++ leetcode/Python/arrays/sortarray_parity.py
@@ -7,6 +7,25 @@
 
 # nums = [0] => [0]
 
+# NOTES
+#   create two arrays to store odds and evens
+#   create a pointer for the start and pointer for the end
+#   loop through the array and check if the nums are odd or even and append them to their corresponding array
+
+def sortArrayInPlace(nums):
+    start_pointer, end_pointer = 0, len(nums) - 1
+
+    while start_pointer <= end_pointer:
+        if nums[start_pointer] % 2 == 0:
+            # if the num is even, we can just skip it and move to the next num
+            start_pointer += 1
+        else:
+            nums[start_pointer], nums[end_pointer] = nums[end_pointer], nums[start_pointer]
+            # decrement end_pointer to avoid infinite loop
+            end_pointer -= 1
+    return nums
+
+
 def sortArrayByParity(nums):
     odd = []
     even = []
@@ -24,8 +43,7 @@ def sortArrayByParity(nums):
         elif last_num % 2 == 0:
             even.append(last_num)
             end_pointer -= 1
-
-        if first_num % 2 != 0:
+        elif first_num % 2 != 0:
             odd.append(first_num)
             start_pointer += 1
         elif last_num % 2 != 0:
@@ -33,3 +51,10 @@ def sortArrayByParity(nums):
             end_pointer -= 1
 
     return even + odd
+
+# Time Complexity
+# O(n)
+
+# Space Complexity
+# O(n^2) ?
+#   created two new arrays, but they're only created once so is it still O(1)
