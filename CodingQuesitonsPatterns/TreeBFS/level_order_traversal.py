b442a182fb4f1b0140cfd27b378c0c6a9cc66bcd
Caro <55109108+CaroMen@users.noreply.github.com>
2021-08-14 22:15:17 -0500

    level order traversal

---
+++ CodingQuesitonsPatterns/TreeBFS/level_order_traversal.py
@@ -0,0 +1,30 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+    def traverse(root):
+        result = []
+        queue = [root]
+
+        if root is None:
+            return result
+
+        while len(queue) > 0:
+            level_length = len(queue)
+            current_level = []
+
+            for _ in range(level_length):
+                current_node = queue.pop(0)
+
+                current_level.append(current_node.val)
+
+                if current_node.left:
+                    queue.append(current_node.left)
+
+                if current_node.right:
+                    queue.append(current_node.right)
+
+            result.append(current_level)
+
+        return result
